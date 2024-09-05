f8f88d73b6bf904b6a5bc6396a13e527874a603a
microhawk <2256553825@qq.com>
2024-09-05 17:29:38 +0800

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,6 +30,7 @@ function customRehypeLazyLoadImage() {
 
 export default defineConfig({
   site: 'https://microhawk.github.io',
+  base: '/ericluo-blog',
   integrations: [sitemap(), tailwind(), solid(), expressiveCode({
     plugins: [pluginLineNumbers(), pluginCollapsibleSections()],
     themes: ["github-dark", "github-light"],
